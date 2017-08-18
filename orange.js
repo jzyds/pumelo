0f130d3199ff0c5470327da630a9b81dacfe8510
xqLi <dyermaker@163.com>
2017-08-18 17:41:34 +0800

    add ajax function

--- orange.js
+++ orange.js
@@ -51,5 +51,74 @@ var orange = {
             })
         }
         return list.sort()
+    },
+
+    //xmlHttpRequest
+    Ajax: function (options){
+        var url = options.url || '',
+            method = options.method.toLowerCase() || 'get',
+            data = options.data || '',
+            callback = options.callback || '',
+            async = options.async || true,
+            header = options.header || ''
+
+        //get请求
+        if(method == 'get'){
+            if(typeof data == 'object'){
+                data = [];
+                for (var k in data){
+                    data.push(k + '=' + data[k]);
+                    data.join('&');
+                }
+            }
+            url += (url.indexOf('?' == -1) ? '?' : '') + data;
+        }
+
+        //post请求
+        if(method == 'post'){
+            if(typeof data == 'object'){
+                var arrs = [];
+                for (var k in data){
+                    arrs.push(k + '=' + data[k]);
+                }
+                data = arrs.join('&');
+            }
+        }
+
+        //创建发送请求
+        var xhr = window.XMLHttpRequest ?  new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP'); //兼容低版本ie
+        xhr.open(method,url,async);
+        if(method == 'post'){
+            //setRequestHeader
+            if(header == ''){
+                xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded')
+            }else{
+                header(xhr);                
+            }
+            xhr.send(data);
+        }else{
+            xhr.send(null);
+        }
+        //异步请求
+        if(async == true){
+            xhr.onreadystatechange = function(){
+              if(xhr.readyState == 4){
+                  callcall();
+              }
+            }
+        }
+        //同步请求
+        if(async == false){
+            callcall();
+        }
+        //返回状态判断
+        function callcall(){
+            if(xhr.status == 200){
+                return callback(JSON.parse(xhr.responseText));
+            }else{
+                return callback(xhr.status + xhr.statusText);
+            }
+        }
     }
+
 };
