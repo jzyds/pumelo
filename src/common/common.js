0fe7e61f06d64cc8d408189932c48d748d77c2a7
xqLi <dyermaker@163.com>
2019-06-18 12:53:53 +0800

    common

--- src/common/common.js
+++ src/common/common.js
@@ -1,4 +1,21 @@
 /**
+ * Update or create query string
+ * @param {String} uri
+ * @param {String} key
+ * @param {String} value
+ * @returns {String}
+ */
+module.exports.updateQueryStringParameter = function(uri, key, value) {
+  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  if (uri.match(re)) {
+    return uri.replace(re, '$1' + key + "=" + value + '$2');
+  } else {
+    return uri + separator + key + "=" + value;
+  }
+};
+
+/**
  * timeDown
  * @param {Number} totalSeconds
  * @param {Function} fn
