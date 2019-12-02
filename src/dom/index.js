b59e47378ebf6f53cd79efbe92d737a40502eb92
xqli <xiaoqiangli.mail@gmail.com>
2019-12-02 15:00:39 +0800

    2.2.4

--- src/dom/index.js
+++ src/dom/index.js
@@ -1,52 +1,36 @@
-// TODO:
-// node_walk: walk the element tree, stop when func(node) returns false
-function node_walk (node, func) {
-  var result = func (node);
-  for (
-    node = node.firstChild;
-    result !== false && node;
-    node = node.nextSibling
-  )
-    result = node_walk (node, func);
-  return result;
-}
+/**
+ * set browser cookie
+ * @returns {void}
+ */
+module.exports = function setCookie () {
+  var Days = 30;
+  var exp = new Date ();
+  exp.setTime (exp.getTime () + Days * 24 * 60 * 60 * 1000);
+  document.cookie =
+    name + '=' + escape (value) + ';expires=' + exp.toGMTString ();
+};
 
-// getCaretPosition: return [start, end] as offsets to elem.textContent that
-//   correspond to the selected portion of text
-//   (if start == end, caret is at given position and no text is selected)
-function getCaretPosition (elem) {
-  var sel = window.getSelection ();
-  var cum_length = [0, 0];
+/**
+ * get browser cookie
+ * @returns {void}
+ */
+module.exports = function getCookie () {
+  var arr, reg = new RegExp ('(^| )' + name + '=([^;]*)(;|$)');
+  return (arr = document.cookie.match (reg)) ? unescape (arr[2]) : null;
+};
 
-  if (sel.anchorNode == elem) cum_length = [sel.anchorOffset, sel.extentOffset];
-  else {
-    var nodes_to_find = [sel.anchorNode, sel.extentNode];
-    if (!elem.contains (sel.anchorNode) || !elem.contains (sel.extentNode))
-      return undefined;
-    else {
-      var found = [0, 0];
-      var i;
-      node_walk (elem, function (node) {
-        for (i = 0; i < 2; i++) {
-          if (node == nodes_to_find[i]) {
-            found[i] = true;
-            if (found[i == 0 ? 1 : 0]) return false; // all done
-          }
-        }
-
-        if (node.textContent && !node.firstChild) {
-          for (i = 0; i < 2; i++) {
-            if (!found[i]) cum_length[i] += node.textContent.length;
-          }
-        }
-      });
-      cum_length[0] += sel.anchorOffset;
-      cum_length[1] += sel.extentOffset;
-    }
+/**
+ * delete browser cookie
+ * @returns {void}
+ */
+module.exports = function deleteCookie () {
+  var exp = new Date ();
+  exp.setTime (exp.getTime () - 1);
+  var cval = getCookie (name);
+  if (cval != null) {
+    document.cookie = name + '=' + cval + ';expires=' + exp.toGMTString ();
   }
-  if (cum_length[0] <= cum_length[1]) return cum_length;
-  return [cum_length[1], cum_length[0]];
-}
+};
 
 /**
  * Check if the browser supports the webp format
@@ -577,3 +561,53 @@ module.exports.browserObj = {
     return navigator.userAgent.match (/Alipay/i);
   },
 };
+
+// TODO:
+// node_walk: walk the element tree, stop when func(node) returns false
+function node_walk (node, func) {
+  var result = func (node);
+  for (
+    node = node.firstChild;
+    result !== false && node;
+    node = node.nextSibling
+  )
+    result = node_walk (node, func);
+  return result;
+}
+
+// getCaretPosition: return [start, end] as offsets to elem.textContent that
+//   correspond to the selected portion of text
+//   (if start == end, caret is at given position and no text is selected)
+function getCaretPosition (elem) {
+  var sel = window.getSelection ();
+  var cum_length = [0, 0];
+
+  if (sel.anchorNode == elem) cum_length = [sel.anchorOffset, sel.extentOffset];
+  else {
+    var nodes_to_find = [sel.anchorNode, sel.extentNode];
+    if (!elem.contains (sel.anchorNode) || !elem.contains (sel.extentNode))
+      return undefined;
+    else {
+      var found = [0, 0];
+      var i;
+      node_walk (elem, function (node) {
+        for (i = 0; i < 2; i++) {
+          if (node == nodes_to_find[i]) {
+            found[i] = true;
+            if (found[i == 0 ? 1 : 0]) return false; // all done
+          }
+        }
+
+        if (node.textContent && !node.firstChild) {
+          for (i = 0; i < 2; i++) {
+            if (!found[i]) cum_length[i] += node.textContent.length;
+          }
+        }
+      });
+      cum_length[0] += sel.anchorOffset;
+      cum_length[1] += sel.extentOffset;
+    }
+  }
+  if (cum_length[0] <= cum_length[1]) return cum_length;
+  return [cum_length[1], cum_length[0]];
+}
